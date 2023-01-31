15e2870f74a55c4928606f48b8b4126a4bfd0223
Narasimha997 <gkumar301099@gmail.com>
2023-01-31 19:28:52 +0530

    JavaScript

---
+++ Unit - 3 Multiple Values/Objects in JavaScript/working with Objects and Arrays/practice.js
@@ -0,0 +1,7 @@
+let people={friends:[]}
+console.log(people);
+let friend1={first:'one',last:'one',id:1}
+let friend2={first:'two',last:'two',id:2}
+let friend3={first:'three',last:'three',id:3}
+people.friends.push(friend1,friend2,friend3)
+console.log(people);
